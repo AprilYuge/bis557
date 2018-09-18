16bafba09adc883bd08609c0f8fe1dcebd4aec41
April <yuge.wang@yale.edu>
2018-09-18 15:08:37 -0400

    HW1 Version3

--- R/linear-models.r
+++ R/linear-models.r
@@ -14,35 +14,35 @@
 #' @export
 linear_model <- function(formula, data) {
   # Get the design matrix X
-  x <- model.matrix(formula,data)
+  x <- model.matrix(formula, data)
   
   # Get the dependent variable y
-  model <- model.frame(formula,data) 
-  y <- model[,1]
+  model <- model.frame(formula, data) 
+  y <- model[, 1]
   
   # QR decomposition of design matrix
   qrr <- qr(x)
   qrr$tol <- 1e-07
   
   # Generate coefficients
-  co <- qr.coef(qrr,y)
+  co <- qr.coef(qrr, y)
   
   # Generate residuals
-  re <- qr.resid(qrr,y)
+  re <- qr.resid(qrr, y)
   names(re) <- 1:length(re)
   
   # Generate effects
-  eff <- qr.qty(qrr,y)
+  eff <- qr.qty(qrr, y)
   nameeff <- colnames(x)
   nameeff <- nameeff[qrr$pivot[1:qrr$rank]]
   names(eff) <- c(nameeff, rep("", length(eff)-length(nameeff)))
   
   # Generate fitted values
-  fit <- qr.fitted(qrr,y)
+  fit <- qr.fitted(qrr, y)
   names(fit) <- 1: length(fit)
   
   #Terms of the model
-  tm <- attr(model,"terms")
+  tm <- attr(model, "terms")
   
   #Form the list
   li <- list()
@@ -51,14 +51,15 @@ linear_model <- function(formula, data) {
   li$effects <- eff
   li$rank <- qrr$rank
   li$fitted.values <- fit
-  li$assign <- attr(x,"assign")
+  li$assign <- attr(x, "assign")
   li$qr <- qrr
   li$df.residual <- nrow(x)-qrr$rank
-  li$contrasts <- attr(x,"contrasts")
-  li$xlevels <- .getXlevels(tm,model)
+  li$contrasts <- attr(x, "contrasts")
+  li$xlevels <- .getXlevels(tm, model)
   li$call <- match.call()
   li$terms <- tm
   li$model <- model
+  li$na.action <- attr(model, "na.action")
   class(li) <- "lm"
   li
 }
