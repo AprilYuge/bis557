525d4d48b531ec09c49ee43ad8cb1dbb8184fda8
April <yuge.wang@yale.edu>
2018-09-18 13:37:01 -0400

    HW1 Version3

--- R/linear-models.r
+++ R/linear-models.r
@@ -15,24 +15,34 @@
 linear_model <- function(formula, data) {
   # To get the design matrix X
   x <- model.matrix(formula,data)
+  
   # To get the dependent variable y
   model <- model.frame(formula,data) 
   y <- model[,1]
+  
   #QR decomposition of design matrix
   qrr <- qr(x)
   qrr$tol <- 1e-07
+  
   #Terms of the model
   tm <- attr(model,"terms")
+  
+  #Effects of the model
+  eff <- qr.qty(qrr,y)
+  nameeff <- colnames(x)
+  nameeff <- nameeff[qrr$pivot[1:qrr$rank]]
+  names(eff) <- c(nameeff, rep("", length(eff)-length(nameeff)))
+  
   #Form the list
   li <- list()
     li$coefficients <- qr.coef(qrr,y)
   li$residuals <- qr.resid(qrr,y)
-  li$effects <- NULL
+  li$effects <- eff
   li$rank <- qrr$rank
   li$fitted.values <- qr.fitted(qrr,y)
   li$assign <- attr(x,"assign")
   li$qr <- qrr
-  li$df.residual <- nrow(x)-ncol(x)
+  li$df.residual <- nrow(x)-qrr$rank
   li$contrasts <- attr(x,"contrasts")
   li$xlevels <- .getXlevels(tm,model)
   li$call <- match.call()
